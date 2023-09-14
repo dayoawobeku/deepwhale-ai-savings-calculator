325622d81d628414cc1e84eb49306857fa44c67b
Dayo <dayo@dojah.io>
2023-09-14 10:58:15 +0100

    handled errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,40 +17,42 @@ export default async function Home() {
   const {data, error} = await supabase.from('on_demand_services').select('*');
 
   return (
-    <main className="flex flex-col">
-      <div className="flex items-end justify-between gap-6 flex-wrap">
-        <h1 className="text-4xl md:text-5xl font-bold text-grey">
-          How much can I save?
-        </h1>
-        <div className="flex items-center gap-6 text-sm text-grey font-medium [&>a]:transition-all [&>a]:duration-300 uppercase">
-          <a
-            href="https://www.linkedin.com/company/deepwhale-ai/?viewAsMember=true"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-grey-700"
-          >
-            <span>LinkedIn</span>
-          </a>
-          <a
-            href="https://twitter.com/biringer_"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-grey-700"
-          >
-            <span>X (TWITTER)</span>
-          </a>
-          <a
-            href="mailto:y.acoine@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-grey-700"
-          >
-            <span>Email</span>
-          </a>
+    <>
+      <main className="flex flex-col">
+        <div className="flex items-end justify-between gap-6 flex-wrap">
+          <h1 className="text-4xl md:text-5xl font-bold text-grey">
+            How much can I save?
+          </h1>
+          <div className="flex items-center gap-6 text-sm text-grey font-medium [&>a]:transition-all [&>a]:duration-300 uppercase">
+            <a
+              href="https://www.linkedin.com/company/deepwhale-ai/?viewAsMember=true"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-grey-700"
+            >
+              <span>LinkedIn</span>
+            </a>
+            <a
+              href="https://twitter.com/biringer_"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-grey-700"
+            >
+              <span>X (TWITTER)</span>
+            </a>
+            <a
+              href="mailto:y.acoine@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-grey-700"
+            >
+              <span>Email</span>
+            </a>
+          </div>
         </div>
-      </div>
 
-      <Form pricingData={data || []} />
-    </main>
+        <Form pricingData={data || []} error={error} />
+      </main>
+    </>
   );
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -3,6 +3,8 @@
 import {useState, useEffect, useMemo} from 'react';
 import Image from 'next/image';
 import Select from 'react-select';
+import toast, {Toaster} from 'react-hot-toast';
+import {PostgrestError} from '@supabase/supabase-js';
 import {PricingDataItem} from './types';
 import {costIc, savingsIc} from '@/assets/images';
 import {DropdownIndicator, customStyles} from './custom-select';
@@ -22,7 +24,13 @@ function getDurationText(unit: string, value: number) {
   }
 }
 
-export default function Form({pricingData}: {pricingData: PricingDataItem[]}) {
+export default function Form({
+  pricingData,
+  error,
+}: {
+  pricingData: PricingDataItem[];
+  error: PostgrestError | null;
+}) {
   const [quantity, setQuantity] = useState(8);
   const [savings, setSavings] = useState(0);
   const [serviceUsage, setServiceUsage] = useState({value: 3, unit: 'months'});
@@ -76,8 +84,12 @@ export default function Form({pricingData}: {pricingData: PricingDataItem[]}) {
         value: uniquePricingData[0].usage_type as string,
         label: uniquePricingData[0].usage_type as string,
       });
+
+      if (error) {
+        toast.error(error.message);
+      }
     }
-  }, [pricingData, usageType]);
+  }, [error, pricingData, usageType]);
 
   const handleServiceUsageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = parseFloat(e.target.value);
@@ -101,125 +113,130 @@ export default function Form({pricingData}: {pricingData: PricingDataItem[]}) {
   }, [pricingData]);
 
   return (
-    <div className="mt-10 lg:mt-[88px] flex flex-col lg:flex-row gap-10 items-start justify-between">
-      <form className="lg:max-w-[373px] w-full">
-        <label htmlFor="usageType" className="font-medium text-grey-700">
-          What usage type will you be using?
-        </label>
-        <Select
-          inputId="usageType"
-          value={usageType}
-          onChange={value =>
-            setUsageType(
-              value as {
-                value: string;
-                label: string;
-              },
-            )
-          }
-          options={options}
-          placeholder="Select usage type"
-          components={{DropdownIndicator, IndicatorSeparator: null}}
-          classNamePrefix="react-select"
-          className="relative mt-4 w-full border-none font-medium text-grey-900"
-          styles={customStyles}
-          required
-          aria-label="Your answer"
-        />
-        <label
-          htmlFor="duration"
-          className="block mt-10 text-grey-700 font-medium"
-        >
-          How long will you use this service?
-        </label>
-        <div className="flex items-center gap-5 justify-between">
-          <input
-            id="duration"
-            type="number"
-            className="lg:max-w-[177px] w-full mt-[3px] h-[45px] text-grey text-base font-medium px-0 rounded-none outline-none border-b-[0.5px] border-grey-500 placeholder:text-sm placeholder:text-grey-700 focus:border-grey focus:ring-0 focus:text-grey bg-white-800"
-            min="0"
-            placeholder="Enter a number"
-            defaultValue={serviceUsage.value}
-            onChange={handleServiceUsageChange}
+    <>
+      <Toaster />
+      <div className="mt-10 lg:mt-[88px] flex flex-col lg:flex-row gap-10 items-start justify-between">
+        <form className="lg:max-w-[373px] w-full">
+          <label htmlFor="usageType" className="font-medium text-grey-700">
+            What usage type will you be using?
+          </label>
+          <Select
+            inputId="usageType"
+            value={usageType}
+            onChange={value =>
+              setUsageType(
+                value as {
+                  value: string;
+                  label: string;
+                },
+              )
+            }
+            options={options}
+            placeholder="Select usage type"
+            components={{DropdownIndicator, IndicatorSeparator: null}}
+            classNamePrefix="react-select"
+            className="relative mt-4 w-full border-none font-medium text-grey-900"
+            styles={customStyles}
+            required
+            aria-label="Your answer"
           />
-          <select
-            name="unit"
-            id="unit"
-            onChange={handleUnitChange}
-            value={serviceUsage.unit}
-            className="lg:max-w-[177px] w-full mt-[3px] h-[45px] text-grey text-base font-medium px-0 rounded-none outline-none border-b-[0.5px] border-grey-500 placeholder:text-sm placeholder:text-grey-700 focus:border-grey focus:ring-0 focus:text-grey bg-white-800 pl-3"
+          <label
+            htmlFor="duration"
+            className="block mt-10 text-grey-700 font-medium"
           >
-            <option value="hours">hours</option>
-            <option value="weeks">weeks</option>
-            <option value="months">months</option>
-            <option value="years">years</option>
-          </select>
-        </div>
-        <label
-          htmlFor="quantity"
-          className="block mt-10 text-grey-700 font-medium"
-        >
-          How many units of this service do you need?
+            How long will you use this service?
+          </label>
           <div className="flex items-center gap-5 justify-between">
             <input
-              id="quantity"
-              name="quantity"
+              id="duration"
               type="number"
-              className="max-w-full w-full mt-[3px] h-[45px] text-grey text-base font-medium px-0 rounded-none outline-none border-b-[0.5px] border-grey-500 placeholder:text-sm placeholder:text-grey-700 focus:border-grey focus:ring-0 focus:text-grey bg-white-800"
+              className="lg:max-w-[177px] w-full mt-[3px] h-[45px] text-grey text-base font-medium px-0 rounded-none outline-none border-b-[0.5px] border-grey-500 placeholder:text-sm placeholder:text-grey-700 focus:border-grey focus:ring-0 focus:text-grey bg-white-800"
               min="0"
               placeholder="Enter a number"
-              defaultValue={quantity}
-              onChange={handleQuantityChange}
+              defaultValue={serviceUsage.value}
+              onChange={handleServiceUsageChange}
             />
+            <select
+              name="unit"
+              id="unit"
+              onChange={handleUnitChange}
+              value={serviceUsage.unit}
+              className="lg:max-w-[177px] w-full mt-[3px] h-[45px] text-grey text-base font-medium px-0 rounded-none outline-none border-b-[0.5px] border-grey-500 placeholder:text-sm placeholder:text-grey-700 focus:border-grey focus:ring-0 focus:text-grey bg-white-800 pl-3"
+            >
+              <option value="hours">hours</option>
+              <option value="weeks">weeks</option>
+              <option value="months">months</option>
+              <option value="years">years</option>
+            </select>
           </div>
-        </label>
-      </form>
-
-      <div className="w-full max-w-full lg:max-w-[540px] xl:max-w-[619px] bg-white p-4 sm:p-8 xl:p-[60px] rounded-xl shadow-[0px_15px_48px_-20px_rgba(0,0,0,0.24)]">
-        <div className="mt-4 flex flex-col items-center">
-          <p className="text-grey-700 text-sm font-medium">TOTAL SAVINGS</p>
-          <p className="text-[2.5rem] text-grey font-bold mt-3 leading-none">
-            ${savings}
-          </p>
-          <p className="mt-6 text-grey-700 font-medium max-w-[300px] text-center">
-            Using {quantity} {quantity === 1 ? 'unit' : 'units'} of this service
-            for
-            <span className="block">
-              {serviceUsage.value}{' '}
-              {getDurationText(serviceUsage.unit, serviceUsage.value)} will save
-              you ${savings}!
-            </span>
-          </p>
-
-          <div className="my-10 h-[1px] w-full bg-grey-400" />
-          <div className="flex flex-col gap-4 w-full">
-            <div className="p-6 bg-white-900 flex items-center gap-2 rounded-lg">
-              <Image src={savingsIc} alt="" width={40} height={40} />
-              <div className="flex flex-col gap-1">
-                <p className="text-grey-700 text-xs font-medium">
-                  TOTAL SAVINGS
-                </p>
-                <p className="text-grey font-bold">${savings}</p>
-              </div>
-            </div>
-            <div className="p-6 bg-white-900 flex items-center gap-2 rounded-lg">
-              <Image src={costIc} alt="" width={40} height={40} />
-              <div className="flex flex-col gap-1">
-                <p className="text-grey-700 text-xs font-medium">TOTAL COST</p>
-                <p className="text-grey font-bold">${cost}</p>
-              </div>
-            </div>
-          </div>
-          <a
-            href="https://app.deepwhale.ai/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center justify-center mt-12 h-12 px-9 bg-grey text-white font-bold rounded-lg text-sm transition-all duration-300 hover:bg-opacity-90 focus:bg-opacity-90"
+          <label
+            htmlFor="quantity"
+            className="block mt-10 text-grey-700 font-medium"
           >
-            Start saving now
-          </a>
+            How many units of this service do you need?
+            <div className="flex items-center gap-5 justify-between">
+              <input
+                id="quantity"
+                name="quantity"
+                type="number"
+                className="max-w-full w-full mt-[3px] h-[45px] text-grey text-base font-medium px-0 rounded-none outline-none border-b-[0.5px] border-grey-500 placeholder:text-sm placeholder:text-grey-700 focus:border-grey focus:ring-0 focus:text-grey bg-white-800"
+                min="0"
+                placeholder="Enter a number"
+                defaultValue={quantity}
+                onChange={handleQuantityChange}
+              />
+            </div>
+          </label>
+        </form>
+
+        <div className="w-full max-w-full lg:max-w-[540px] xl:max-w-[619px] bg-white p-4 sm:p-8 xl:p-[60px] rounded-xl shadow-[0px_15px_48px_-20px_rgba(0,0,0,0.24)]">
+          <div className="mt-4 flex flex-col items-center">
+            <p className="text-grey-700 text-sm font-medium">TOTAL SAVINGS</p>
+            <p className="text-[2.5rem] text-grey font-bold mt-3 leading-none">
+              ${savings}
+            </p>
+            <p className="mt-6 text-grey-700 font-medium max-w-[300px] text-center">
+              Using {quantity} {quantity === 1 ? 'unit' : 'units'} of this
+              service for
+              <span className="block">
+                {serviceUsage.value}{' '}
+                {getDurationText(serviceUsage.unit, serviceUsage.value)} will
+                save you ${savings}!
+              </span>
+            </p>
+
+            <div className="my-10 h-[1px] w-full bg-grey-400" />
+            <div className="flex flex-col gap-4 w-full">
+              <div className="p-6 bg-white-900 flex items-center gap-2 rounded-lg">
+                <Image src={savingsIc} alt="" width={40} height={40} />
+                <div className="flex flex-col gap-1">
+                  <p className="text-grey-700 text-xs font-medium">
+                    TOTAL SAVINGS
+                  </p>
+                  <p className="text-grey font-bold">${savings}</p>
+                </div>
+              </div>
+              <div className="p-6 bg-white-900 flex items-center gap-2 rounded-lg">
+                <Image src={costIc} alt="" width={40} height={40} />
+                <div className="flex flex-col gap-1">
+                  <p className="text-grey-700 text-xs font-medium">
+                    TOTAL COST
+                  </p>
+                  <p className="text-grey font-bold">${cost}</p>
+                </div>
+              </div>
+            </div>
+            <a
+              href="https://app.deepwhale.ai/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-center mt-12 h-12 px-9 bg-grey text-white font-bold rounded-lg text-sm transition-all duration-300 hover:bg-opacity-90 focus:bg-opacity-90"
+            >
+              Start saving now
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
